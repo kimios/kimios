476e9907a4f985ea5ff1b19d661ebcc4016287a8
Fabien Alin <f@devlib.fr>
2013-10-27 21:11:33 +0100

    [KMS-267] Fix ConverterImpl Temporary file path generation

--- kimios-convert/src/main/java/org/kimios/kernel/converter/ConverterImpl.java
+++ kimios-convert/src/main/java/org/kimios/kernel/converter/ConverterImpl.java
@@ -8,6 +8,7 @@ import org.kimios.utils.configuration.ConfigurationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.List;
 
 public abstract class ConverterImpl implements Converter {
@@ -17,6 +18,13 @@ public abstract class ConverterImpl implements Converter {
 
     protected ConverterImpl() {
         temporaryRepository = ConfigurationManager.getValue(Config.DEFAULT_TEMPORARY_PATH);
+        try{
+            File file = new File(temporaryRepository);
+            if(!file.exists())
+                file.mkdirs();
+        } catch (Exception e){
+            log.error("Error while creating temp repository converter", e);
+        }
         log.debug("Calling " + this.getClass().getName() + " converter implementation...");
     }
 
