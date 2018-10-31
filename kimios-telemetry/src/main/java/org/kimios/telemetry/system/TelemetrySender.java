d491b766e2e8881b00b4adfd4351398cf77e8ee5
Fabien Alin <fabien.alin@gmail.com>
2018-10-31 16:08:30 +0100

    [kimios-telemetry] fix default admin informations

--- kimios-telemetry/src/main/java/org/kimios/telemetry/system/TelemetrySender.java
+++ kimios-telemetry/src/main/java/org/kimios/telemetry/system/TelemetrySender.java
@@ -1,6 +1,8 @@
 package org.kimios.telemetry.system;
 
+import org.apache.commons.lang.StringUtils;
 import org.kimios.kernel.controller.ISecurityController;
+import org.kimios.kernel.deployment.DataInitializerCtrl;
 import org.kimios.kernel.security.model.Session;
 import org.kimios.telemetry.controller.CustomScheduledThreadPoolExecutor;
 import org.kimios.telemetry.controller.ITelemetryController;
@@ -57,7 +59,18 @@ public class TelemetrySender implements Runnable {
     public void run() {
         if (this.telemetryController.getUuid() != null) {
             try {
-                Session session = this.securityController.startSession("admin", "kimios");
+
+                String defaultDomain =
+                        StringUtils.isEmpty(System.getenv(DataInitializerCtrl.KIMIOS_DEFAULT_DOMAIN)) ?
+                                (StringUtils.isEmpty(System.getProperty("kimios.default.domain")) ? "kimios" : System.getProperty("kimios.default.domain")) :
+                                System.getenv(DataInitializerCtrl.KIMIOS_DEFAULT_DOMAIN);
+
+                String adminLogin =
+                        StringUtils.isEmpty(System.getenv(DataInitializerCtrl.KIMIOS_ADMIN_USERID)) ?
+                                (StringUtils.isEmpty(System.getProperty("kimios.admin.userid")) ? "admin" : System.getProperty("kimios.admin.userid")) :
+                                System.getenv(DataInitializerCtrl.KIMIOS_ADMIN_USERID);
+
+                Session session = this.securityController.startSession(adminLogin, defaultDomain);
                 TelemetrySenderJob job = new TelemetrySenderJob(this.telemetryController, session);
                 this.customScheduledThreadPoolExecutor.scheduleAtFixedRate(job, 0, 1, TimeUnit.MINUTES);
             } catch (Exception e) {
