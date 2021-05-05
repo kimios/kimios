6600b89f542563da60c43d92b60a66e61e240291
devtom30 <devtom30@gmail.com>
2021-05-05 11:21:40 +0200

    new module: kimios-websocket

---
+++ kimios-websocket/src/main/java/org/kimios/websocket/websocket/MessageEncoder.java
@@ -0,0 +1,29 @@
+package org.kimios.websocket.websocket;
+
+import com.google.gson.Gson;
+import org.kimios.websocket.model.Message;
+
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+public class MessageEncoder implements Encoder.Text<Message> {
+
+    private static Gson gson = new Gson();
+
+    @Override
+    public String encode(Message message) throws EncodeException {
+        String json = gson.toJson(message);
+        return json;
+    }
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+        // Custom initialization logic
+    }
+
+    @Override
+    public void destroy() {
+        // Close resources
+    }
+}
